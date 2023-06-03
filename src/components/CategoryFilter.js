c37b78afa02dbdeb17f0263e6e5877e841646886
Brodie Ashcraft <brodiea19@gmail.com>
2023-06-03 16:11:46 -0700

    No tests passing, deliverables for Filter, TaskList, and task working

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, setFilter }) {
+  function handleClick(e) {
+    setFilter(e.target.innerText);
+    setCategoryButtons(
+      categories.map((cat) => {
+        let className = cat === e.target.innerText ? "selected" : "";
+        return (
+          <button key={cat} className={className} onClick={handleClick}>
+            {cat}
+          </button>
+        );
+      })
+    );
+  }
+
+  const [categoryButtons, setCategoryButtons] = useState(
+    categories.map((cat) => {
+      return (
+        <button key={cat} onClick={handleClick}>
+          {cat}
+        </button>
+      );
+    })
+  );
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categoryButtons}
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import Task from "./Task";
 
-function TaskList() {
-  return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
-  );
+function TaskList({ tasks, filter }) {
+  const [newTaskList, setNewTasks] = useState(tasks);
+
+  function handleRemoveTask(event) {
+    setNewTasks(
+      newTaskList.filter((task) => {
+        return task.text !== event.target.parentElement.children[1].innerText;
+      })
+    );
+  }
+
+  const ourTasks = newTaskList.filter((task) => {
+    if (filter === "All") {
+      return true;
+    }
+    if (filter === "") {
+      return true;
+    }
+    return task.category === filter;
+  });
+
+  const taskList = ourTasks.map((task) => {
+    return <Task key={task.text} text={task.text} category={task.category} handleRemoveTask={handleRemoveTask} />;
+  });
+
+  return <div className="tasks">{taskList}</div>;
 }
 
 export default TaskList;
